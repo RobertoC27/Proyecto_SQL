243ec438e26e50f32631ad03bb53b4f914f889be
RobertoC27 <chi13027@uvg.edu.gt>
2016-03-29 08:56:56 -0600

    agregados los metodos de entero,char,fecha,decimal; falta llenar el cuerpo de los mismos

--- CompiladorSQL/src/compiladorsql/NuestroVisitor.java
+++ CompiladorSQL/src/compiladorsql/NuestroVisitor.java
@@ -10,5 +10,40 @@ package compiladorsql;
  * @author user
  */
 public class NuestroVisitor<T> extends GramaticaBaseVisitor{
+
+    @Override
+    public T visitValue(GramaticaParser.ValueContext ctx) {
+        
+        return (T)visitChildren(ctx);
+    }
+
+    @Override
+    public Object visitEntero(GramaticaParser.EnteroContext ctx) {
+        boolean bNeg=false;
+        if(ctx.MINUS()!=null){
+            bNeg=true;
+        }
+        
+        ctx.NUM();
+        return super.visitEntero(ctx); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public Object visitDecimal(GramaticaParser.DecimalContext ctx) {
+        return super.visitDecimal(ctx); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object visitFecha(GramaticaParser.FechaContext ctx) {
+        return super.visitFecha(ctx); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object visitCharacter(GramaticaParser.CharacterContext ctx) {
+        return super.visitCharacter(ctx); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
     
 }
